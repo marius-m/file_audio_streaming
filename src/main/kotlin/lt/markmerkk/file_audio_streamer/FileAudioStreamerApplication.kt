95c2df59e61226be97e1fe40801c5c8543917a9d
Marius Merkevicius <marius.merkevicius@treatwell.com>
2019-01-01 09:01:17 +0200

    Init

---
+++ src/main/kotlin/lt/markmerkk/file_audio_streamer/FileAudioStreamerApplication.kt
@@ -0,0 +1,12 @@
+package lt.markmerkk.file_audio_streamer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FileAudioStreamerApplication
+
+fun main(args: Array<String>) {
+	runApplication<FileAudioStreamerApplication>(*args)
+}
+
---
+++ src/test/kotlin/lt/markmerkk/file_audio_streamer/FileAudioStreamerApplicationTests.kt
@@ -0,0 +1,17 @@
+package lt.markmerkk.file_audio_streamer
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class FileAudioStreamerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+

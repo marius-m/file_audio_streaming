ea9013d2b376332c76cc3756b644b62e088b960f
Marius Merkevicius <marius.m@ito.lt>
2021-09-23 23:34:53 +0300

    Provide context path

---
+++ src/main/kotlin/lt/markmerkk/file_audio_streamer/BuildConfig.kt
@@ -0,0 +1,6 @@
+package lt.markmerkk.file_audio_streamer
+
+data class BuildConfig(
+    val version: String,
+    val contextPath: String
+)
--- src/main/kotlin/lt/markmerkk/file_audio_streamer/configs/ComponentsConfig.kt
+++ src/main/kotlin/lt/markmerkk/file_audio_streamer/configs/ComponentsConfig.kt
@@ -2,6 +2,7 @@ package lt.markmerkk.file_audio_streamer.configs
 
 import io.sentry.Sentry
 import io.sentry.SentryClient
+import lt.markmerkk.file_audio_streamer.BuildConfig
 import lt.markmerkk.file_audio_streamer.UUIDGen
 import lt.markmerkk.file_audio_streamer.daos.BookDao
 import lt.markmerkk.file_audio_streamer.daos.CategoryDao
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.context.annotation.PropertySource
+import org.springframework.context.annotation.Scope
 import org.springframework.core.env.Environment
 import org.springframework.core.io.ResourceLoader
 
@@ -23,6 +25,21 @@ import org.springframework.core.io.ResourceLoader
 class ComponentsConfig {
 
     @Bean
+    @Scope("singleton")
+    open fun provideBuildConfig(
+        @Value("\${version}") version: String,
+        @Value("\${dockerHost}") dockerHost: String,
+        @Value("#{servletContext.contextPath}") contextPath: String
+    ): BuildConfig {
+        val buildConfig = BuildConfig(
+            version = version,
+            contextPath = contextPath
+        )
+        return buildConfig
+    }
+
+    @Bean
+    @Scope("singleton")
     open fun provideUUID(
             env: Environment
     ): UUIDGen {
@@ -30,6 +47,7 @@ class ComponentsConfig {
     }
 
     @Bean
+    @Scope("singleton")
     open fun provideCredentials(
             env: Environment
     ): Credentials {
@@ -40,6 +58,7 @@ class ComponentsConfig {
     }
 
     @Bean
+    @Scope("singleton")
     open fun provideFsConfig(
             @Value("\${rootPaths}") rootPaths: String
     ): FSSource {
@@ -47,6 +66,7 @@ class ComponentsConfig {
     }
 
     @Bean
+    @Scope("singleton")
     open fun fsInteractor(
             resourceLoader: ResourceLoader
     ): FSInteractor {
@@ -54,6 +74,7 @@ class ComponentsConfig {
     }
 
     @Bean
+    @Scope("singleton")
     open fun bookRepository(
             fsInteractor: FSInteractor,
             fsSource: FSSource,
@@ -74,12 +95,14 @@ class ComponentsConfig {
 
     @Bean
     @Profile("prod")
+    @Scope("singleton")
     open fun sentryClientProd(): SentryClient {
         return Sentry.init("https://948d91b168824f27a0490a1484b692c3@o348125.ingest.sentry.io/5289085")
     }
 
     @Bean
     @Profile("dev")
+    @Scope("singleton")
     open fun sentryClientDev(): SentryClient {
         return Sentry.init()
     }
